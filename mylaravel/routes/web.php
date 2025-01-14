752c85ddc8946c7036515cc7301d9b24055e21f3
TorlapSayHi <torlap.ritchai@gmail.com>
2025-01-14 19:09:23 +0700

    Laravel Template

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -3,10 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 use App\Http\Controllers\NewMyController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', 
+    [LoginController::class, 'index']);
+    
+Route::get('/register', 
+    [RegisterController::class, 'index']);
+
+Route::get('/home', 
+    [HomeController::class, 'index']);
 
 Route::get('/hello', function () {
     return "<h1>Hellow World!!!!!</h1>";
@@ -23,3 +35,7 @@ Route::get("/newmycontroller/{id?}",
 
 Route::post("/newmycontroller/{id?}", 
 [NewMyController::class, 'multiplicationtable']);
+
+Route::get('/', function (){
+    return view('layouts.default');
+});
